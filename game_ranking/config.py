efc0115171211c4b5f188293ab239eb891f7ea04
Rasika Chockalingam <rasika.chockalingam@gmail.com>
2026-01-28 20:28:35 +0800

    Altered Config file to point to wthin repo

--- game_ranking/config.py
+++ game_ranking/config.py
@@ -1,4 +1,4 @@
-CSV_STEAM = r'C:\Users\Rasika\Desktop\AGS\repos\game_ranking\raw_files\raw_steam.csv'
-CSV_NON_STEAM = r'C:\Users\Rasika\Desktop\AGS\repos\game_ranking\raw_files\raw_non_steam.csv'
-DEV_LIST = r'C:\Users\Rasika\Desktop\AGS\repos\game_ranking\src\data\developer_list.xlsx'
-GENRE_LIST = r'C:\Users\Rasika\Desktop\AGS\repos\game_ranking\src\data\genre_list.xlsx'
+CSV_STEAM = r'raw_files\raw_steam.csv'
+CSV_NON_STEAM = r'raw_files\raw_non_steam.csv'
+DEV_LIST = r'src\data\developer_list.xlsx'
+GENRE_LIST = r'src\data\genre_list.xlsx'
