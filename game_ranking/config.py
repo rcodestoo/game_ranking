2a74280fa7956366c36cd4b3cf9f282a2fbc65ca
Rasika Chockalingam <rasika.chockalingam@gmail.com>
2026-01-28 20:37:12 +0800

    Added Pathlib for proper routing for source files

--- game_ranking/config.py
+++ game_ranking/config.py
@@ -1,4 +1,14 @@
-CSV_STEAM = r'raw_files\raw_steam.csv'
-CSV_NON_STEAM = r'raw_files\raw_non_steam.csv'
-DEV_LIST = r'src\data\developer_list.xlsx'
-GENRE_LIST = r'src\data\genre_list.xlsx'
+from pathlib import Path
+
+# BASE_DIR is the folder containing THIS script
+BASE_DIR = Path(__file__).resolve().parent
+
+# Define directories using forward slashes (works on both) or as parts
+RAW_DIR = BASE_DIR / 'raw_files'
+DATA_DIR = BASE_DIR / 'src' / 'data'
+
+# Define files
+CSV_STEAM = RAW_DIR / 'raw_steam.csv'
+CSV_NON_STEAM = RAW_DIR / 'raw_steam.csv'
+DEV_LIST = DATA_DIR / 'developer_list.xlsx'
+GENRE_LIST = DATA_DIR / 'genre_list.xlsx'
